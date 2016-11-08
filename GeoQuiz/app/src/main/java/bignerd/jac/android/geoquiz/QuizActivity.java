fb105f7e9cab6b8308edf235bd4d35f810f44dd2
wasonte <wasonte@gmail.com>
2016-11-08 18:17:20 +0100

    Chapter 1 End

--- GeoQuiz/app/src/main/java/bignerd/jac/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/bignerd/jac/android/geoquiz/QuizActivity.java
@@ -2,12 +2,35 @@ package bignerd.jac.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, "True", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, "False", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
