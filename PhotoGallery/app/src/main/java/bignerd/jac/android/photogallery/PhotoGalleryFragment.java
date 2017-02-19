f0fe023e1c23b50ea833062b1a1a710daec64b88
jalcolea <jorge.alcolea@ciber.com>
2017-02-19 13:44:22 +0100

    Chapter 23 p413

---
+++ PhotoGallery/app/src/main/java/bignerd/jac/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,57 @@
+package bignerd.jac.android.photogallery;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+
+/**
+ * Created by jalcolea on 19/02/2017.
+ */
+public class PhotoGalleryFragment extends Fragment {
+
+	private static final String TAG = "PhotoGalleryFragment";
+
+	private RecyclerView mPhotoRecyclerView;
+
+	public static PhotoGalleryFragment newInstance(){
+		return new PhotoGalleryFragment();
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+		new FetchItemsTask().execute();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+		mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+		mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+		return view;
+	}
+
+	private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
+				Log.i(TAG, "Fetched contents of URL: " + result);
+			} catch (IOException ioe) {
+				Log.e(TAG, "Failed to fetch URL: ", ioe);
+			}
+			return null;
+		}
+	}
+}
---
+++ PhotoGallery/app/src/main/java/bignerd/jac/android/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package bignerd.jac.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jorge.alcolea on 03/01/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    public abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
